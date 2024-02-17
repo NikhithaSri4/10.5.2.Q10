263ad04725ce1c55ed3ac9696655bf517b5345a6
NikhithaSri4 <155454226+NikhithaSri4@users.noreply.github.com>
2024-02-17 18:19:00 +0530

    Add files via upload

--- 10.5.2.10/codes/g2.py
+++ 10.5.2.10/codes/g2.py
@@ -1,15 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load data from the file
+
 data = np.loadtxt('values.dat')
 n = data[:, 0].astype(int)
 xn = data[:, 1]
 
-# Plot the stem plot
 plt.stem(n, xn)
-
-# Add labels and title
 plt.xlabel('n')
 plt.ylabel('x(n)')
 plt.grid(True)
